02e859b8119cb6736f62f339ffa3989021a2a28d
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2020-06-05 14:29:21 -0300

    Updates

---
+++ precache-manifest.ec938385256c4b9526d108da75d4cb33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8024fc9c47a4a39e5aa1d409ac87474",
+    "url": "/projeto_react/index.html"
+  },
+  {
+    "revision": "3085163ef762fed3da17",
+    "url": "/projeto_react/static/css/main.cf047875.chunk.css"
+  },
+  {
+    "revision": "0d77fdc14a063fedda20",
+    "url": "/projeto_react/static/js/2.f3352394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projeto_react/static/js/2.f3352394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3085163ef762fed3da17",
+    "url": "/projeto_react/static/js/main.6d549295.chunk.js"
+  },
+  {
+    "revision": "e263218f769cb43d4e45",
+    "url": "/projeto_react/static/js/runtime-main.212aaa8b.js"
+  },
+  {
+    "revision": "e035dc20ba18ede5f11a72211471e0bb",
+    "url": "/projeto_react/static/media/calculator.e035dc20.png"
+  },
+  {
+    "revision": "95a02ef329c96123c4b06813726d706d",
+    "url": "/projeto_react/static/media/github_api.95a02ef3.PNG"
+  },
+  {
+    "revision": "1356df22ba23208f61cb838395bdf40d",
+    "url": "/projeto_react/static/media/tasklist.1356df22.png"
+  }
+]);
